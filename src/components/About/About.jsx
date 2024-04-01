0d9d6de66fa6b6362273ddb76b98f74657d48229
Saïf <53183335+saifbacari@users.noreply.github.com>
2024-04-01 19:21:08 +0200

    skeleton about done + basic config react-router-dom built

---
+++ src/components/About/About.jsx
@@ -0,0 +1,15 @@
+import Searchbar from '../UI/Searchbar/Searchbar'
+import styles from './About.module.css'
+
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+        <p>Logo</p>
+        <Searchbar />
+    </div>
+  )
+}
+
+
+export default About
