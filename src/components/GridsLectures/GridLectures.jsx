50c7f84db488004767d22a37bc41c044d473d455
Saïf <53183335+saifbacari@users.noreply.github.com>
2024-04-13 16:29:18 +0200

    frame lecture building done

--- src/components/GridsLectures/GridLectures.jsx
+++ src/components/GridsLectures/GridLectures.jsx
@@ -1,12 +1,38 @@
+import LectureItem from "../Lecture/LectureItem";
+import styles from "./GridLectures.module.css";
+
 const GridLectures = () => {
-    return (
-      <>
-        <div>Liste des dernières lectures</div>
-        <div>Lectures ajoutées</div>
-        <div>Quelques lectures des favoris</div>
-      </>
-    )
-  }
-  
-  export default GridLectures;
-  
+  return (
+    <>
+      <div className={styles.gridLectures}>
+      <h1 className={styles.titleLectures}>Dernières lectures lus</h1>
+        <div className={styles.gridLastLectures}>
+            <LectureItem />
+            <LectureItem />
+            <LectureItem />
+            <LectureItem />
+       
+        </div>
+        <h1 className={styles.titleLectures}>Dernières lectures ajoutés</h1>
+        <div className={styles.gridAddedLectures}>
+          
+            <LectureItem />
+            <LectureItem />
+            <LectureItem />
+            <LectureItem />
+           
+
+          </div>
+          <h1 className={styles.titleLectures}>Lectures ajoutés dans Favoris</h1>
+        <div className={styles.gridAddedLecturesToFavorites}>
+            <LectureItem />
+            <LectureItem />
+            <LectureItem />
+            <LectureItem />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GridLectures;
